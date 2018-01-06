5a3e79b68f24560e385258e96e2cf3b15e042c71
Brendan Long <self@brendanlong.com>
2018-01-06 16:11:56 -0500

    Handle empty rows

--- src/xlsx_parser.ml
+++ src/xlsx_parser.ml
@@ -2,11 +2,15 @@ open Base
 open Printf
 
 let find_attr (attrs : (string * string) list) (name_to_find : string) =
-  List.find_map_exn attrs ~f:(fun (name, value) ->
+  List.find_map attrs ~f:(fun (name, value) ->
     if String.(name = name_to_find) then
       Some value
     else
       None)
+
+let find_attr_exn attrs name_to_find =
+  find_attr attrs name_to_find
+  |> Option.value_exn ~here:[%here]
 
 module Workbook_view = struct
   type t =
@@ -20,10 +24,10 @@ module Workbook_view = struct
     let open Xml in
     function
     | Element ("workbookView", attrs, _) ->
-      let x_window = find_attr attrs "xWindow" |> Int.of_string in
-      let y_window = find_attr attrs "yWindow" |> Int.of_string in
-      let window_width = find_attr attrs "windowWidth" |> Int.of_string in
-      let window_height = find_attr attrs "windowHeight" |> Int.of_string in
+      let x_window = find_attr_exn attrs "xWindow" |> Int.of_string in
+      let y_window = find_attr_exn attrs "yWindow" |> Int.of_string in
+      let window_width = find_attr_exn attrs "windowWidth" |> Int.of_string in
+      let window_height = find_attr_exn attrs "windowHeight" |> Int.of_string in
       Some { x_window ; y_window ; window_width ; window_height }
     | _ -> None
 end
@@ -38,8 +42,8 @@ module Sheet_meta = struct
     let open Xml in
     function
     | Element ("sheet", attrs, _) ->
-      let name = find_attr attrs "name" in
-      let id = find_attr attrs "sheetId" |> Int.of_string in
+      let name = find_attr_exn attrs "name" in
+      let id = find_attr_exn attrs "sheetId" |> Int.of_string in
       Some { name ; id }
     | _ -> None
 end
@@ -114,26 +118,59 @@ let read_file filename =
         |> Xml.parse_string
         |> let open Xml in function
         | Element ("worksheet", _, children) ->
-          List.find_exn children ~f:(function
-            | Element ("sheetData", _, _) -> true
-            | _ -> false)
-          |> Xml.children
-          |> List.filter_map ~f:(function
-          | Element ("row", _, cells) ->
-            List.filter_map cells ~f:(function
-              | Element ("c", attrs, [ Element ("v", _, [ PCData v ]) ]) ->
-                let is_shared = List.exists attrs ~f:(function
-                  | "t", "s" -> true
-                  | _ -> false)
-                in
-                if is_shared then
-                  let i = Int.of_string v in
-                  Some shared_strings.(i)
-                else
-                  Some v
-              | _ -> None)
-            |> Option.return
-          | _ -> None)
+          let num_cols =
+            List.find_map_exn children ~f:(function
+            | Element ("cols", _, cols) ->
+              List.filter_map cols ~f:(function
+                | Element ("col", attrs, _) ->
+                  find_attr attrs "max" |> Option.map ~f:Int.of_string
+                | _ -> None)
+              |> Option.return
+            | _ -> None)
+            |> List.max_elt ~cmp:Int.compare
+            |> Option.value ~default:0
+          in
+          let row_map =
+            List.find_map children ~f:(function
+            | Element ("sheetData", _, children) -> Some children
+            | _ -> None)
+            |> Option.value ~default:[]
+            |> List.filter_map ~f:(function
+            | Element ("row", attrs, cells) ->
+              let index = find_attr_exn attrs "r" |> Int.of_string in
+              let row =
+                List.filter_map cells ~f:(function
+                | Element ("c", attrs, children) ->
+                  (match children with
+                  | [] -> Some ""
+                  | [ Element ("v", _, [ PCData v ]) ] ->
+                    let is_shared = List.exists attrs ~f:(function
+                      | "t", "s" -> true
+                      | _ -> false)
+                    in
+                    if is_shared then
+                      let i = Int.of_string v in
+                      Some shared_strings.(i)
+                    else
+                      Some v
+                  | _ -> None)
+                | _ -> None)
+              in
+              (* Rows at 1-indexed, convert to 0-indexed *)
+              Some (index - 1, row)
+            | _ -> None)
+            |> Map.Using_comparator.of_alist_exn ~comparator:Int.comparator
+          in
+          let n =
+            Map.keys row_map
+            |> List.max_elt ~cmp:Int.compare
+            |> Option.value ~default:0
+          in
+          let default_row = List.init num_cols ~f:(Fn.const "") in
+          List.init n ~f:Fn.id
+          |> List.map ~f:(fun i ->
+            Map.find row_map i
+            |> Option.value ~default:default_row)
         | _ -> assert false
       in
       { name ; rows }))
